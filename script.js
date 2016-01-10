ab382421632c441afa628b53589bd3dc9e50b8a5
Rachael Robinson <Rachael@Rachaels-MacBook-Pro.local>
2016-01-10 11:53:42 -0800

    added more hover attributes and working on toggle navbar dropdown

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ $(document).ready(function(){
 	});
 	$("#about").click(function(){
 		console.log("clicked");
-		autoScrollTo('restOfPage');
-		// restOfPage
+		$('html,body').animate({
+        	scrollTop: $("#restOfPage").offset().top},
+        	'slow');
 	});
 	$("#github").click(function(){
 		window.location.href='https://github.com/rachaelrobinson';
@@ -18,7 +19,10 @@ $(document).ready(function(){
 		window.location.href='./photography.html';
 	});
 	$("#img-arrow").click(function(){
-		autoScrollTo("github-link");
+		$('html,body').animate({
+        	scrollTop: $("#github-link").offset().top},
+        	'slow');
+		// autoScrollTo("github-link");
 	});
 	$("#github-link").click(function(){
 		window.location.href='https://github.com/rachaelrobinson';
@@ -32,56 +36,65 @@ $(document).ready(function(){
 
 
 });
-	window.onload = function() {
-		setTimeout (function () {
-			scrollTo(0,0);
-		}, 100); //100ms for example
-	}
-	var scrollY = 0;
-	var distance = 1;
-	var dist = 30;
-	var speed = 30;
 
+// $("button").click(function() {
+//     $('html,body').animate({
+//         scrollTop: $(".second").offset().top},
+//         $(window).scrollTop($('').offset().top);
 
-window.onload = function() {
-	setTimeout (function () {
-		scrollTo(0,0);
-	}, 100); //100ms for example
-}
-	function autoScrollTo(el) {
-		console.log(el);
-		var currentY = window.pageYOffset;
-		var targetY = document.getElementById(el).offsetHeight;
-		console.log(targetY);
-		var bodyHeight = document.body.offsetHeight;
-		var yPos = currentY + window.innerHeight;
-		var animator = setTimeout('autoScrollTo(\'' + el + '\')', 24);
-		console.log(yPos, bodyHeight);
-		if (yPos > bodyHeight) {
-			clearTimeout(animator);
-		} 
-		else {
-		    if (currentY < targetY - distance) {
-		    	scrollY = currentY + dist;
-		    	window.scroll(0, scrollY);
-		    } 
-		    else {
-		      	clearTimeout(animator);
-		    }
-		}
-	}
-
-	function resetScroller(el) {
-		var currentY = window.pageYOffset;
-		var targetY = document.getElementById(el).offsetTop;
-		var animator = setTimeout('resetScroller(\'' + el + '\')', speed);
-		if (currentY > targetY) {
-		    scrollY = currentY - distance;
-		    window.scroll(0, scrollY);
-		}
-		else {
-			clearTimeout(animator);
-		}
-	}
-
+//         'slow');
 // });
+// 	window.onload = function() {
+// 		setTimeout (function () {
+// 			scrollTo(0,0);
+// 		}, 100); //100ms for example
+// 	}
+// 	var scrollY = 0;
+// 	var distance = 1;
+// 	var dist = 10;
+// 	var speed = 4;
+
+
+// window.onload = function() {
+// 	setTimeout (function () {
+// 		scrollTo(0,0);
+// 	}, 100); //100ms for example
+// }
+// 	function autoScrollTo(el) {
+// 		console.log(el);
+// 		var currentY = window.pageYOffset;
+// 		var targetY = document.getElementById(el).offsetHeight;
+// 		console.log(targetY);
+// 		var bodyHeight = document.body.offsetHeight;
+// 		var yPos = currentY + window.innerHeight;
+// 		var animator = setTimeout('autoScrollTo(\'' + el + '\')', 24);
+// 		console.log(yPos, bodyHeight);
+// 		if (yPos > bodyHeight) {
+// 			clearTimeout(animator);
+// 		} 
+// 		else {
+// 		    if (currentY < targetY - distance) {
+// 		    	currentY = currentY + 183;
+// 		    	scrollY = currentY + dist;
+// 		    	window.scroll(0, scrollY);
+// 		    } 
+// 		    else {
+// 		      	clearTimeout(animator);
+// 		    }
+// 		}
+// 	}
+
+// 	function resetScroller(el) {
+// 		var currentY = window.pageYOffset;
+// 		var targetY = document.getElementById(el).offsetTop;
+// 		var animator = setTimeout('resetScroller(\'' + el + '\')', speed);
+// 		if (currentY > targetY) {
+// 		    scrollY = currentY - distance;
+// 		    window.scroll(0, scrollY);
+// 		}
+// 		else {
+// 			clearTimeout(animator);
+// 		}
+// 	}
+
+// // });
